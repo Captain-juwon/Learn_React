5e5a9e4740ab2ded27052268737f615864fb4f43
Captain-juwon <106703460+Captain-juwon@users.noreply.github.com>
2023-06-08 23:34:52 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import './App.css';
+import microsoft01 from './microsoft01.webp';
+
+
+function App() {
+  
+  return (
+    
+    <div className="App">
+
+      <div className='App-div'>
+        <div className='Head'>
+          <h1 className='Heading'>Projects</h1>
+          <a href='#' className='Link'>contact me</a>
+        </div>
+
+        <div className='Card-div'>
+            <div>
+              <a href='#'><img src={microsoft01} alt='microsoft01 picture' className='Img' /></a>
+              
+              <p>DESIGN PORTFOLIO</p>
+              
+              <div className='Label'>
+                  <span>HTML</span>
+                  <span>CSS</span>
+              </div>
+            </div>
+            
+            <div>
+              <a href='#'><img src={microsoft01} alt='microsoft01 picture' className='Img' /></a>
+              
+              <p>DESIGN PORTFOLIO</p>
+              
+              <div className='Label'>
+                  <span>HTML</span>
+                  <span>CSS</span>
+              </div>
+            </div>
+            
+            <div>
+              <a href='#'><img src={microsoft01} alt='microsoft01 picture' className='Img' /></a>
+              
+              <p>DESIGN PORTFOLIO</p>
+              
+              <div className='Label'>
+                  <span>HTML</span>
+                  <span>CSS</span>
+                  <span>JAVASCRIPT</span>
+              </div>
+            </div>
+            
+            <div>
+              <a href='#'><img src={microsoft01} alt='microsoft01 picture' className='Img' /></a>
+              
+              <p>DESIGN PORTFOLIO</p>
+              
+              <div className='Label'>
+                  <span>HTML</span>
+                  <span>CSS</span>
+                  <span>JAVASCRIPT</span>
+              </div>
+            </div>
+            
+            <div>
+              <a href='#'><img src={microsoft01} alt='microsoft01 picture' className='Img' /></a>
+              
+              <p>DESIGN PORTFOLIO</p>
+              
+              <div className='Label'>
+                  <span>HTML</span>
+                  <span>CSS</span>
+                  <span>JAVASCRIPT</span>
+              </div>
+            </div>
+            
+            <div>
+              <a href='#'><img src={microsoft01} alt='microsoft01 picture' className='Img' /></a>
+              
+              <p>DESIGN PORTFOLIO</p>
+              
+              <div className='Label'>
+                  <span>HTML</span>
+                  <span>CSS</span>
+                  <span>JAVASCRIPT</span>
+              </div>
+            </div>
+        
+        </div>
+
+      </div>
+
+    </div>
+  
+  );
+
+}
+
+export default App;
