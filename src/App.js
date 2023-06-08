a3665dc42f334f78772ccaba9c89a399e2848edc
Captain-juwon <106703460+Captain-juwon@users.noreply.github.com>
2023-06-08 23:49:17 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import microsoft01 from './microsoft01.webp';
+import microsoft02 from './microsoft02.webp';
+import microsoft03 from './microsoft03.webp';
+import microsoft04 from './microsoft04.webp';
+import microsoft05 from './microsoft05.webp';
+import microsoft06 from './microsoft06.webp';
 
 
 function App() {
@@ -27,7 +32,7 @@ function App() {
             </div>
             
             <div>
-              <a href='#'><img src={microsoft01} alt='microsoft01 picture' className='Img' /></a>
+              <a href='#'><img src={microsoft02} alt='microsoft02 picture' className='Img' /></a>
               
               <p>DESIGN PORTFOLIO</p>
               
@@ -38,19 +43,7 @@ function App() {
             </div>
             
             <div>
-              <a href='#'><img src={microsoft01} alt='microsoft01 picture' className='Img' /></a>
-              
-              <p>DESIGN PORTFOLIO</p>
-              
-              <div className='Label'>
-                  <span>HTML</span>
-                  <span>CSS</span>
-                  <span>JAVASCRIPT</span>
-              </div>
-            </div>
-            
-            <div>
-              <a href='#'><img src={microsoft01} alt='microsoft01 picture' className='Img' /></a>
+              <a href='#'><img src={microsoft03} alt='microsoft03 picture' className='Img' /></a>
               
               <p>DESIGN PORTFOLIO</p>
               
@@ -62,7 +55,7 @@ function App() {
             </div>
             
             <div>
-              <a href='#'><img src={microsoft01} alt='microsoft01 picture' className='Img' /></a>
+              <a href='#'><img src={microsoft04} alt='microsoft04 picture' className='Img' /></a>
               
               <p>DESIGN PORTFOLIO</p>
               
@@ -74,7 +67,19 @@ function App() {
             </div>
             
             <div>
-              <a href='#'><img src={microsoft01} alt='microsoft01 picture' className='Img' /></a>
+              <a href='#'><img src={microsoft05} alt='microsoft05 picture' className='Img' /></a>
+              
+              <p>DESIGN PORTFOLIO</p>
+              
+              <div className='Label'>
+                  <span>HTML</span>
+                  <span>CSS</span>
+                  <span>JAVASCRIPT</span>
+              </div>
+            </div>
+            
+            <div>
+              <a href='#'><img src={microsoft06} alt='microsoft06 picture' className='Img' /></a>
               
               <p>DESIGN PORTFOLIO</p>
               
